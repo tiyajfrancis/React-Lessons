995e5cb2469e9e8f0102128834e138fc40b590ae
tiyajfrancis <tjf31102002@gmail.com>
2022-06-30 13:12:39 +0400

    simple static page done

--- project 1/index.js
+++ project 1/index.js
@@ -1,46 +1,32 @@
-function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+// import { createRoot } from 'react-dom/client'
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled" href="#">Disabled</a>
-                </li>
-                </ul>
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-        </nav>
-    )
-}
-
-ReactDOM.render(
-<div>
-    <Navbar />
-
-</div>, 
-document.getElementById("root"))
+const page = (
+    <div>
+        <img src="./react-logo.png" width="40px"/>
+        <h1>Fun Facts About React</h1>
+        <ol>
+            <li>
+                was first releeased in 2013
+            </li>
+            <li>
+                was originally created by Jordan Walke
+            </li>
+            <li>
+                has weel over 100K stars on Github
+            </li>
+            <li>
+                is maintained by Facebook
+            </li>
+            <li>
+                Powers thousands of enterprise apps, including mobile apps
+            </li>
+        </ol>
+    </div>
+ )
+//  const container = document.getElementById("root");
+//  const root = createRoot(container); 
+//  root.render(page)
+ ReactDOM.render(
+    page,
+    document.getElementById("root")
+ )
